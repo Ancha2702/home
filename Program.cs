4532d59831f283bb4352c6cea4189470ed543eef
Ancha2702 <113132853+Ancha2702@users.noreply.github.com>
2022-09-18 00:17:11 +0300

    четвертое

--- Program.cs
+++ Program.cs
@@ -1,16 +1,15 @@
-﻿/* Напишите программу, которая на вход принимает число и выдаёт,
- является ли число чётным (делится ли оно на два без остатка).*/
+﻿/*Напишите программу, которая на вход принимает число (N),
+ а на выходе показывает все чётные числа от 1 до N.*/
 Console.Clear();
-//Вводим  число
-Console.WriteLine("Введите любое число:");
+//Вводим число
+Console.WriteLine("Введите число:");
 String input = Console.ReadLine();
 int num1 = int.Parse(input);
- if (num1%2 == 0)
- {
-    Console.WriteLine("Число четное");
- }
- else 
- {
- Console.WriteLine("Число нечетное");
- }
-
+ for (int i = 0; i <= num1; i++)
+    {
+    if (i%2 == 0)
+        {
+    Console.Write(i + " ");
+        } 
+    }
+    Console.WriteLine(" ");
