698463b6d88e7ed7b748373f69846d382dc7c662
Ancha2702 <113132853+Ancha2702@users.noreply.github.com>
2022-09-18 00:14:30 +0300

    второе задание

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
-﻿/* Напишите программу, которая на вход принимает
-два числа и выдаёт, какое число большее, а какое меньшее.*/
+﻿/*  Напишите программу, которая принимает на вход 
+три числа и выдаёт максимальное из этих чисел.*/
+Console.Clear();
 //Вводим первое число
 Console.WriteLine("Введите первое число:");
 String input = Console.ReadLine();
@@ -8,11 +9,25 @@ int num1 = int.Parse(input);
 Console.WriteLine("Введите второе число:");
 String input2 = Console.ReadLine();
 int num2 = int.Parse(input2);
+//Вводим  третье число
+Console.WriteLine("Введите третье число:");
+String input3 = Console.ReadLine();
+int num3 = int.Parse(input3);
+
 if (num1 > num2)
-{
+{ 
+ if (num1 > num3)
      Console.WriteLine($"Максимальное число: {num1}");
 }
-else
+else if (num2 > num3)
 {
     Console.WriteLine($"Максимальное число: {num2}");
 }
+else if (num3 > num1)
+   {
+    Console.WriteLine($"Максимальное число: {num3}");
+   }
+   else
+   {
+    Console.WriteLine("Некорректные данные или числа равны");
+   }
