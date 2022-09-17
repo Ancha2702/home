f619d5279def0b5cb45cdfe81c633c9570bc465b
Ancha2702 <113132853+Ancha2702@users.noreply.github.com>
2022-09-18 00:16:09 +0300

    третье

--- Program.cs
+++ Program.cs
@@ -1,33 +1,16 @@
-﻿/*  Напишите программу, которая принимает на вход 
-три числа и выдаёт максимальное из этих чисел.*/
+﻿/* Напишите программу, которая на вход принимает число и выдаёт,
+ является ли число чётным (делится ли оно на два без остатка).*/
 Console.Clear();
-//Вводим первое число
-Console.WriteLine("Введите первое число:");
+//Вводим  число
+Console.WriteLine("Введите любое число:");
 String input = Console.ReadLine();
 int num1 = int.Parse(input);
-//Вводим второе число
-Console.WriteLine("Введите второе число:");
-String input2 = Console.ReadLine();
-int num2 = int.Parse(input2);
-//Вводим  третье число
-Console.WriteLine("Введите третье число:");
-String input3 = Console.ReadLine();
-int num3 = int.Parse(input3);
+ if (num1%2 == 0)
+ {
+    Console.WriteLine("Число четное");
+ }
+ else 
+ {
+ Console.WriteLine("Число нечетное");
+ }
 
-if (num1 > num2)
-{ 
- if (num1 > num3)
-     Console.WriteLine($"Максимальное число: {num1}");
-}
-else if (num2 > num3)
-{
-    Console.WriteLine($"Максимальное число: {num2}");
-}
-else if (num3 > num1)
-   {
-    Console.WriteLine($"Максимальное число: {num3}");
-   }
-   else
-   {
-    Console.WriteLine("Некорректные данные или числа равны");
-   }
